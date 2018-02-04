cbfd98ffa52418d5c8922537e5d92ad2e5707ca5
Val <valeriy.rotan@gmail.com>
2018-02-03 22:09:33 -0800

    added some gsheets stuff

---
+++ submit/main.js
@@ -0,0 +1,14 @@
+$('document').ready(function() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzJb8ch1pg5vSCx8dAoTHEQwwn5SjfcGsWa7GpP4mb4-C20tq0G/exec'
+    const form = document.forms['submit-to-google-sheet']
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, {
+                method: 'POST',
+                body: new FormData(form)
+            })
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+    })
+});
