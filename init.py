776b2f54523e736eac6132a098ea8f228087711c
valrotan <valeriy.rotan@gmail.com>
2018-02-06 14:52:45 -0800

    removed python code

--- init.py
+++
@@ -1,6 +0,0 @@
-from SimpleHTTPServer import *
-
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('169.254.48.173', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
