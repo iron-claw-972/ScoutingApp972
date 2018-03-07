3f570eeea15cc1acea248359477f4f2db810448d
Val <valeriy.rotan@gmail.com>
2018-03-06 20:08:58 -0800

    added inc/dec-rement buttons and stuff

--- submit/submit.js
+++ submit/submit.js
@@ -1,7 +1,7 @@
 $('document').ready(function() {
     const scriptURL = 'https://script.google.com/macros/s/AKfycbzJb8ch1pg5vSCx8dAoTHEQwwn5SjfcGsWa7GpP4mb4-C20tq0G/exec'
     const form = document.forms['submit-to-google-sheet']
-    console.log('HERE');
+
     form.addEventListener('submit', e => {
         e.preventDefault()
         fetch(scriptURL, {
@@ -13,6 +13,18 @@ $('document').ready(function() {
                 window.location.replace("../formSubmitted");
             })
             .catch(error => console.error('Error!', error.message))
+    })
+
+    $('.inc').click(function() {
+        var el = $('#' + event.target.getAttribute("data-in"))
+        el.val(parseInt(el.val()) + 1)
+    })
+
+    $('.dec').click(function() {
+        var el = $('#' + event.target.getAttribute("data-in"))
+        if (parseInt(el.val()) <= 0)
+            return
+        el.val(parseInt(el.val()) - 1)
     })
 });
 
